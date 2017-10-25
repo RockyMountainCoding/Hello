7a951c44a59a9f72de087dcfc36197457ff30bda
Gabriel Sabastian Hayes <thegoodtimez@Gabriels-MacBook-Air.local>
2017-10-25 16:57:40 -0600

    Just changing colors of image view

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var greeting: UITextView!
     @IBOutlet weak var mainBackGround: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-print("Hello This is a change i made.")   }
+print("Hello This is a change i made..")   }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -25,19 +26,25 @@ var changeColor = 0
         changeColor = changeColor + 1
         if changeColor == 1{
             mainBackGround.backgroundColor = UIColor.red
+            greeting.text = "These colors are kind of cool!"
+            greeting.backgroundColor = UIColor.red
         }
         if changeColor == 2{
             mainBackGround.backgroundColor = UIColor.green
+            greeting.backgroundColor = UIColor.green
         }
         if changeColor == 3{
             mainBackGround.backgroundColor = UIColor.yellow
+            greeting.backgroundColor = UIColor.yellow
         }
         if changeColor == 4{
             mainBackGround.backgroundColor = UIColor.blue
+            greeting.backgroundColor = UIColor.blue
             
         }
         if changeColor == 5{
             mainBackGround.backgroundColor = UIColor.purple
+            greeting.backgroundColor = UIColor.purple
         }
         if changeColor > 5 {changeColor = changeColor - 6}
 }

87dcf020000a710aad2c2d482395c53cd73c84ae
Gabriel Sabastian Hayes <thegoodtimez@Gabriels-MacBook-Air.local>
2017-10-25 15:46:45 -0600

    added button and image view that will change colors when the button is pressed.

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainBackGround: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,27 @@ print("Hello This is a change i made.")   }
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+var changeColor = 0
+    @IBAction func OHYA(_ sender: Any) {
+        changeColor = changeColor + 1
+        if changeColor == 1{
+            mainBackGround.backgroundColor = UIColor.red
+        }
+        if changeColor == 2{
+            mainBackGround.backgroundColor = UIColor.green
+        }
+        if changeColor == 3{
+            mainBackGround.backgroundColor = UIColor.yellow
+        }
+        if changeColor == 4{
+            mainBackGround.backgroundColor = UIColor.blue
+            
+        }
+        if changeColor == 5{
+            mainBackGround.backgroundColor = UIColor.purple
+        }
+        if changeColor > 5 {changeColor = changeColor - 6}
+}
+    
 }
 
